98a4f28db0a3e2894c29c383a9bba0ec8fa5a898
getshcc <iamshcc@gmail.com>
2023-02-05 12:45:24 +0330

    blank project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <section>
+    <header>
+      <nav style="display: flex; flex-direction: column;">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </header>
+    <RouterView></RouterView>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
