a1ab849250c65f6d1bb46b5a26e58dd9e9ef8b55
getshcc <iamshcc@gmail.com>
2023-02-11 19:54:41 +0330

    Add MainFooter

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import MainHeader from './components/MainHeader.vue';
+import MainFooter from './components/MainFooter.vue';
 </script>
 
 <template>
   <section>
     <MainHeader></MainHeader>
     <RouterView></RouterView>
+    <MainFooter></MainFooter>
   </section>
 </template>
 
