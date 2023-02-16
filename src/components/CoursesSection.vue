d4f1a53fff3c55adb6e7cdf5329c24e224b51ed1
shcc <getshcc@gmail.com>
2023-02-16 11:22:25 +0330

    Fix conflict

--- src/components/CoursesSection.vue
+++ src/components/CoursesSection.vue
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 <script setup>
-
 import CourseCard from './CourseCard.vue';
 import FillVideoIcon from './icons/FillVideoicon.vue';
 import SplitSection from './SplitSection.vue';
@@ -24,33 +22,3 @@ const { courses } = defineProps(['courses'])
 </template>
 
 <style scoped></style>
-=======
-<script setup>
-import CourseCard from './CourseCard.vue';
-import FillVideoIcon from './icons/FillVideoicon.vue';
-import SplitSection from './SplitSection.vue';
-const { courses } = defineProps(['courses'])
-</script>
-
-<template>
-
-    <SplitSection>
-        <template #icon>
-            <FillVideoIcon></FillVideoIcon>
-        </template>
-        <template #title>
-            دوره ها
-        </template>
-        <template #default>
-            <main class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
-                <CourseCard v-for="course in courses" :key="course.id" :course="course"></CourseCard>
-            </main>
-        </template>
-    </SplitSection>
-
-</template>
-
-<style scoped>
-
-</style>
->>>>>>> features/modal
